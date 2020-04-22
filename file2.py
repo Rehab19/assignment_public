a1a81c13c559f5ce152334443ffe217c948bb66f
Rehab <rehabelbermawy19@gmail.com>
2020-04-22 02:17:54 -0400

    Assignment - 1 (if-Statements)

---
+++ file2.py
@@ -0,0 +1,6 @@
+my_name = ("Rehab")
+user_name = input("Enter your first name: ").title()
+if user_name == my_name:
+    print(f"Hello, {user_name}! The password is:@ 12")
+else:
+  print(f"Hello, {user_name}! See you later.")
