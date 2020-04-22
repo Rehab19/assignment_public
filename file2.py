04ccef30f1e4d4eb80828ee55e4e4485e9912c89
Rehab <rehabelbermawy19@gmail.com>
2020-04-22 14:23:29 -0400

    second_commit

--- file2.py
+++ file2.py
@@ -1,4 +1,4 @@
-my_name = ("Rehab")
+my_name = ("Rehab").title()
 user_name = input("Enter your first name: ").title()
 if user_name == my_name:
     print(f"Hello, {user_name}! The password is:@ 12")
