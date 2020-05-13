84488faa11d8290d49614c03bf351f856dad84f9
Rehab <rehabelbermawy19@gmail.com>
2020-05-13 16:21:18 -0400

    first html commit

---
+++ 06_assignment.py
@@ -0,0 +1,12 @@
+n = int(input("please enter a number: "))
+prime_list = []
+for i in range(1, n+1):
+    count = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            count +=1
+    if count == 2:
+        prime_list.append(i)
+print(prime_list)
+
+
